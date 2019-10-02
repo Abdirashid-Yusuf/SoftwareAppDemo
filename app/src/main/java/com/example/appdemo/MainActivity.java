dd58774f21496d1c0a54cfb03539f640b6c34b11
Meddy <Abdibarre24@gmail.com>
2019-10-02 13:59:10 -0400

    Week 5 inclass practice

--- app/src/main/java/com/example/appdemo/MainActivity.java
+++ app/src/main/java/com/example/appdemo/MainActivity.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         Button but = findViewById(R.id.button4);
         but.setOnClickListener(new View.OnClickListener() {
